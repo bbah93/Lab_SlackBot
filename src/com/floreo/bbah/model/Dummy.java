fd1d8aa048adfa8696045a6397fca91469b61f19
Bobby <bobby@floreolabs.org>
2020-10-20 10:29:00 -0400

    Adds Dummy Class

---
+++ src/com/floreo/bbah/model/Dummy.java
@@ -0,0 +1,28 @@
+package com.floreo.bbah.model;
+
+public class Dummy {
+
+    String str;
+    int myInt;
+
+    public Dummy(String str, int myInt) {
+        this.str = str;
+        this.myInt = myInt;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public void setMyInt(int myInt) {
+        this.myInt = myInt;
+    }
+}
