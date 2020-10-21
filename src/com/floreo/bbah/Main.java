2b994dbb0e0727d05d81d4614769ad134eac5c80
andinet <andinetmn@gmail.com>
2020-10-21 18:01:33 -0500

    final test message sent

--- src/com/floreo/bbah/Main.java
+++ src/com/floreo/bbah/Main.java
@@ -16,10 +16,11 @@ public class Main {
 
         myBot.listMessages(Slack.BOTS_CHANNEL_ID);
 
-        //myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg\"");
+        myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg\"");
 
         // Post "Hello, world!" to the #bots channel
-        myBot.sendMessageToBotsChannel("test");
+
+        myBot.sendMessageToBotsChannel("Hello World ");
         //myBot.sendMessageToBotsChannel(" https://fitengezondnu.files.wordpress.com/2014/10/healthy-foods-for-kidneys.jpg");
 
         // Post a pineapple photo to the #bots channel
