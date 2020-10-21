bd35fdce8e4cf316f28644bf709dc2952a3683d5
Student <student@DC-TECH-02.local>
2020-10-21 10:32:39 -0500

    changes

--- src/com/floreo/bbah/model/User.java
+++ src/com/floreo/bbah/model/User.java
@@ -10,105 +10,85 @@ import de.ralleytn.simple.json.JSONObject;
  */
 
 public class User {
-
+    // TODO: implement private fields for each of the following user JSON keys:
     private String id;
     private String name;
     private boolean deleted;
     private String color;
     private String profile;
-    private boolean isAdmin;
-    private boolean isOwner;
-    private boolean isPrimaryOwner;
-    private boolean isRestricted;
-    private boolean isUltraRestricted;
-    private boolean  has2fa;
-    private boolean twoFactorType;
-    private boolean hasFiles;
-
-    public String getId() {
+    private boolean is_admin;
+    private boolean is_owner;
+    private boolean is_primary_owner;
+    private boolean is_restricted;
+    private boolean is_ultra_restricted;
+    private boolean has_2fa;
+    public  User(JSONObject json) {
+        // TODO: parse a user from the incoming json
+        if (json.get("id") != null) {
+            this.id = (String) json.get("id");
+        }
+        if (json.get("name") != null) {
+            this.name = (String) json.get("name");
+        }
+        if (json.get("deleted") != null) {
+            this.deleted = (Boolean) json.get("deleted");
+        }
+        if (json.get("color") != null) {
+            this.id = (String) json.get(" color");
+        }
+        if (json.get("profile") != null) {
+            this.id = (String) json.get("profile");
+        }
+        if (json.get("is_admin") != null) {
+            this.is_admin = (Boolean) json.get("is_admin");
+        }
+        if (json.get("is_owner") != null) {
+            this.is_owner = (Boolean) json.get("is_owner");
+        }
+        if (json.get("is_primary_owner") != null) {
+            this.is_primary_owner = (Boolean) json.get("is_primary_owner");
+        }
+        if (json.get("is_restricted") != null) {
+            this.is_restricted = (Boolean) json.get("is_restricted");
+        }
+        if (json.get("is_ultra_restricted") != null) {
+            this.is_ultra_restricted = (Boolean) json.get("is_ultra_restricted");
+        }
+        if (json.get("has_2fa") != null) {
+            this.has_2fa = (Boolean) json.get("has_2fa");
+        }
+    }
+    public String getId () {
         return id;
     }
-
-    public String getName() {
+    public String getName () {
         return name;
     }
-
-    public boolean isDeleted() {
+    public boolean isDeleted () {
         return deleted;
     }
-
-    public String getColor() {
+    public String getColor () {
         return color;
     }
-
-    public String getProfile() {
+    public String getProfile () {
         return profile;
     }
-
-    public boolean isAdmin() {
-        return isAdmin;
+    public boolean isIs_admin () {
+        return is_admin;
     }
-
-    public boolean isOwner() {
-        return isOwner;
+    public boolean isIs_owner () {
+        return is_owner;
     }
-
-    public boolean isPrimaryOwner() {
-        return isPrimaryOwner;
+    public boolean isIs_primary_owner () {
+        return is_primary_owner;
     }
-
-    public boolean isRestricted() {
-        return isRestricted;
+    public boolean isIs_restricted () {
+        return is_restricted;
     }
-
-    public boolean isUltraRestricted() {
-        return isUltraRestricted;
+    public boolean isIs_ultra_restricted () {
+        return is_ultra_restricted;
     }
-
-    public boolean isHas2fa() {
-        return has2fa;
+    public boolean isHas_2fa () {
+        return has_2fa;
     }
-
-    public boolean isTwoFactorType() {
-        return twoFactorType;
-    }
-
-    public boolean isHasFiles() {
-        return hasFiles;
-    }
-
-    // TODO: implement private fields for each of the following user JSON keys:
-    // "id"
-    // "name"
-    // "deleted"
-    // "color"
-    // "profile"
-    // "is_admin"
-    // "is_owner"
-    // "is_primary_owner"
-    // "is_restricted"
-    // "is_ultra_restricted"
-    // "has_2fa"
-    // "two_factor_type"
-    // "has_files"
-
-    public User(JSONObject json) {
-        // TODO: parse a user from the incoming json
-
-        this.id = json.getString("id");
-        this.name = json.getString("name");
-        this.deleted = json.getBoolean("deleted");
-        this.color = json.getString("color");
-        this.profile = json.getString("profile");
-        this.isAdmin =  json.getBoolean("is_admin");
-        this.isOwner = json.getBoolean("is_owner");
-        this.isPrimaryOwner = json.getBoolean("is_primary_owner");
-        this.isRestricted = json.getBoolean("is_restricted");
-        this.isUltraRestricted = json.getBoolean("is_ultra_restricted");
-        this.has2fa = json.getBoolean("has_2fa");
-        this.twoFactorType = json.getBoolean("two_factor_type");
-        this.hasFiles = json.getBoolean("has_files");
-    }
-
-    // TODO add getters to access private fields
 }
