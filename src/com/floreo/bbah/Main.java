dd33af9dd65e8cb265b4b7c1e1b11ba3ba813bc7
andinet <andinetmn@gmail.com>
2020-10-21 06:36:37 -0500

    api.txt added

--- src/com/floreo/bbah/Main.java
+++ src/com/floreo/bbah/Main.java
@@ -14,12 +14,15 @@ public class Main {
 
         myBot.listMessages(Slack.BOTS_CHANNEL_ID);
 
+        myBot.listMessages(Slack.BOTS_CHANNEL_ID);
+
+        //myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg\"");
 
         // Post "Hello, world!" to the #bots channel
-        //myBot.sendMessage("Hello, world!");
+        myBot.sendMessageToBotsChannel("Hello from theborg!");
+        myBot.sendMessageToBotsChannel(" https://akns-images.eonline.com/eol_images/Entire_Site/2016912/rs_1024x759-161012111018-1024.The-Borg-Star-Trek-The-Next-Generation.ms.101216.jpg");
 
         // Post a pineapple photo to the #bots channel
-        //myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
-
+        // myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
     }
 }
