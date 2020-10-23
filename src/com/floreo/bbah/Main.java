db4f1f8c4a642a7ff6cf2c8973ae6ed16053be32
andinet2019 <50561815+andinet2019@users.noreply.github.com>
2020-10-23 15:37:27 -0500

    new main created on main

---
+++ src/com/floreo/bbah/Main.java
@@ -0,0 +1,33 @@
+package com.floreo.bbah;
+
+import com.floreo.bbah.model.Attachment;
+import com.floreo.bbah.model.User;
+import com.floreo.bbah.network.Slack;
+import de.ralleytn.simple.json.JSONObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bot myBot = new Bot();
+
+        myBot.testApi();
+
+        myBot.listChannels();
+
+        myBot.listMessages(Slack.BOTS_CHANNEL_ID);
+
+        myBot.listMessages(Slack.BOTS_CHANNEL_ID);
+        myBot.sendMessageToBotsChannel("Hello from the theborg 2020");
+        myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg\"");
+    
+        // Post "Hello, world!" to the #bots channel
+
+        myBot.sendMessageToBotsChannel("Just checking from Andinet? https://www.picgifs.com/comment-gifs/f/funny/picgifs-funny-1703861.gif");
+        myBot.deleteMessageInBotsChannel("");
+        //myBot.sendMessageToBotsChannel(" https://fitengezondnu.files.wordpress.com/2014/10/healthy-foods-for-kidneys.jpg");
+
+        // Post a pineapple photo to the #bots channel
+        // myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
+    }
+}
