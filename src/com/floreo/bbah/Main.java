778250618ee0c3830da31cb8483ecea9a578cc99
Student <student@DC-TECH-05.local>
2020-10-23 09:22:34 -0500

    Updates Bot and Main

--- src/com/floreo/bbah/Main.java
+++ src/com/floreo/bbah/Main.java
@@ -23,8 +23,6 @@ public class Main {
          //Post a pineapple photo to the #bots channel
         myBot.sendMessageToBotsChannel("https://www.gannett-cdn.com/-mm-/5af65baa0a70a9741f6b872381028b5d0651db74/c=0-71-3164-1859/local/-/media/2016/05/10/USATODAY/USATODAY/635985036876795120-USATSI-9289181.jpg?width=3164&height=1788&fit=crop&format=pjpg&auto=webp");
 
-        //myBot.sendMessageToBotsChannel(nbaFacts);
-
         myBot.nbaFacts();
 
     }
